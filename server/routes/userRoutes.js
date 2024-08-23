01a5500e1045ea9bd270d07b3326d857b9611f62
AryanAD <aryanhohai@gmail.com>
2024-08-23 16:02:46 +0545

    tested user apis

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,16 @@
 import express from "express";
 
-import { createUser, getAllUsers } from "../controllers/userController.js";
+import {
+  createUser,
+  getAllUsers,
+  loginUser,
+  logoutCurrentUser,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+} from "../controllers/userController.js";
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
@@ -10,5 +20,19 @@ router
   .route("/")
   .post(createUser)
   .get(authenticate, authorizeAdmin, getAllUsers);
+
+router.post("/login", loginUser);
+router.post("/logout", logoutCurrentUser);
+
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
+
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;
