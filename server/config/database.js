ce15bdd6063d280bfb7e1145e09701d29bbcd3ce
AryanAD <aryanhohai@gmail.com>
2024-09-08 22:31:22 +0545

    client backend done

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const dbName = "projectone";
 const dbUserName = "root";
-const dbPassword = "1234";
+const dbPassword = "";
 const dbHost = "localhost";
 
 const sequelize = new Sequelize(dbName, dbUserName, dbPassword, {
